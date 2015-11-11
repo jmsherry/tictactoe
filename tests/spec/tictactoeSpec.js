4039699f4ec579405b23a18294ea07045aea285e
James Sherry <jamessherry@Jamess-MacBook-Pro.local>
2015-11-11 22:56:17 +0000

    beforeEach example

--- tests/spec/tictactoeSpec.js
+++ tests/spec/tictactoeSpec.js
@@ -1,7 +1,17 @@
 describe("TicTacToe", function() {
+  var game, board, squares;
+
+  beforeEach(function(){
+    game = new TicTacToe(3);
+  });
 
   it("should be loaded into the browser and be accessible", function() {
-    expect(window.tictactoe).toBeDefined();
+    expect(window.TicTacToe).toBeDefined();
+  });
+
+  it("should create a board", function(){
+    board = document.getElementsByClassName('board');
+    expect(board.length).toBe(1);
   });
 
 });
