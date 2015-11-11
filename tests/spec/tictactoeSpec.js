cc19dbf538b5cda4278e8cc3b28cc8f03dec18d0
James Sherry <james.m.sherry@googlemail.com>
2015-11-11 23:54:12 +0000

    rearranged to make board accessible using an iife

--- tests/spec/tictactoeSpec.js
+++ tests/spec/tictactoeSpec.js
@@ -1,24 +1,33 @@
 describe("TicTacToe", function() {
-  var game, board, squares;
+  var game, board, squares, sideLength;
 
-  beforeEach(function(){
-    game = new TicTacToe(3);
+  beforeEach(function() {
+    sideLength = 3;
+    game = new TicTacToe(sideLength);
     game.init();
 
-    board = document.getElementsByClassName('board')[0];
+    (function() {
+      board = document.getElementsByClassName('board')[0];
+    }());
+    
   });
 
-  afterEach(function(){
+  afterEach(function() {
     game = null;
     document.body.removeChild(board);
   });
 
-  it("should be loaded into the browser and be accessible", function() {
+  it("It should be loaded into the browser and be accessible", function() {
     expect(window.TicTacToe).toBeDefined();
   });
 
-  it("should create a board", function(){
-    expect(document.getElementsByClassName('board').length).toBe(1);
+  it("It should create a board", function() {
+    expect(board).not.toBeUndefined();
+  });
+
+  it("The board should have 9 squares", function() {
+    //var boards = document.getElementsByClassName('board');
+    expect(board.childNodes.length).toBe(9);
   });
 
 });
