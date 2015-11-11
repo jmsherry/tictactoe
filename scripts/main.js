23a814b98c0644e2fac9749a064b4e69f3153c34
James Sherry <james.m.sherry@googlemail.com>
2015-11-11 23:13:11 +0000

    reorganisation into revealing module pattern and initialising to pass the failing test

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,28 @@
 (function(){
   "use strict";
 
-  window.tictactoe = window.tictactoe || function(){
-    
+  window.TicTacToe = window.TicTacToe || function(sideLength){
+
+    var boardSize;
+
+    sideLength = sideLength | 3;
+    boardSize = sideLength * sideLength;
+
+    function createBoard(){
+      var board = document.createElement("DIV");
+      board.className = "board";
+      document.body.appendChild(board);
+    }
+
+    function init(){
+      createBoard();
+    }
+
+
+    return {
+      init: init
+    };
+
   };
 
 }());
