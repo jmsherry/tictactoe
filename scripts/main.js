fe639e4cf4206c0f098674a06e8bb3813c9fad61
James Sherry <james.m.sherry@googlemail.com>
2015-11-12 00:22:57 +0000

    added the squares to the board

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,32 @@
     boardSize = sideLength * sideLength;
 
     function createBoard(){
-      var board = document.createElement("DIV");
-      board.className = "board";
+      //Create the board
+      var board, squares;
+
+      board = document.createElement("DIV");
+      board.className = "board row";
       document.body.appendChild(board);
+
+      //create the squares
+      squares = [];
+
+      function createSquare(n){ //function will be hoisted
+        var square = document.createElement("DIV");
+        square.className = "col-xs-" + n;
+        square.nodeValue = n;
+        square.style.width = Math.floor(1/sideLength*100)+ "%";
+        return square;
+      }
+
+      for(var i=0; i < boardSize; i+=1){
+        squares.push(createSquare(i));
+      }
+
+      squares.forEach(function(square){
+        document.getElementsByClassName("board")[0].appendChild(square);
+      });
+
     }
 
     function init(){
